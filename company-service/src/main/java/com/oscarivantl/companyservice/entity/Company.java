52fb7e7f31242a291a78b33dfb6bf17278aadad1
Oscar Ivan Torres Lopez <oscar.torres@unipamplona.edu.co>
2024-07-17 20:41:46 -0500

    adding first entity

---
+++ company-service/src/main/java/com/oscarivantl/companyservice/entity/Company.java
@@ -0,0 +1,25 @@
+package com.oscarivantl.companyservice.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String founder;
+    private String logo;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    private LocalDate foundationDate;
+
+}
